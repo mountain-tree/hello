392f0e31befd576d3eb942feba17179bf4d575b2
Helio Frota <00hf11@gmail.com>
2022-11-05 08:40:29 -0300

    interesting

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"example/hello/foo"
 	"fmt"
+	"path"
 
 	"rsc.io/quote"
 )
@@ -10,4 +11,17 @@ import (
 func main() {
     fmt.Println(foo.Bar())
     fmt.Println(quote.Go())
+
+		var dir, file string
+
+		dir, file = path.Split("~/.local")
+
+		fmt.Println("dir:", dir)
+		fmt.Println("file:", file)
+
+		var f string
+		_, f = path.Split("~/.local/")
+		fmt.Println("file-f:", f)
+		_, f = path.Split("~/.local")
+		fmt.Println("file-f:", f)
 }
